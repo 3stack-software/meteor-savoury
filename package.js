9bb2c79b7fbfe6eca3ac8a226cece10a51a21e6e
Nathan Muir <ndmuir@gmail.com>
2015-09-04 14:10:34 +1000

    Bump to version 1.1.0

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: '3stack:savoury',
-  version: '1.0.3',
+  version: '1.1.0',
   summary: 'Reactively show the user the status of their method call.',
   git: 'https://github.com/3stack-software/meteor-savoury',
   documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@0.9.2');
+  api.versionsFrom('METEOR@1.1.0.2');
 
   api.use([
     'coffeescript',
